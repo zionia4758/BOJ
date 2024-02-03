4477735f3409e4e9e973c6c3f720af1bb3cf9e61
zionia4758 <2016025196@hanyang.ac.kr>
2024-02-03 14:50:50 +0900

    자바 폴더 이동

--- 해결-java/16918_봄버맨.java
+++ 해결-java/16918_봄버맨.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -6,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
-public class 봄버맨{
+public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
